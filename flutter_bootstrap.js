d902c7813e7d61d75e500f1630afd684b8329f86
wukongfantastic5-droid <wukongfantastic5@gmail.com>
2026-08-08 20:11:35 +0800

    update flutter_bootstrap.js

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2427430682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3916999973" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
